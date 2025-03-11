f1a8ba8fd5b8a260503a363271b743811f4995c1
Jagger85 <andresfonsortega@yahoo.es>
2025-03-11 13:45:06 +0700

    Feat(blog): Added a blog post template and also an all posts template without any styling

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='blog-index')
+    path('', views.index, name='blog-index'),
+    path('posts/', views.posts, name='all-posts'),
+    path('posts/<str:slug>',views.post)
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'blog/index.html')
+
+def posts(request):
+    return render(request, 'blog/posts.html')
+
+def post(request, slug):
+    return render(request, 'blog/post.html',{'postname':slug})
