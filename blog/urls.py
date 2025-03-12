61858b9e49265237c4f294915f855066cfaa4ac5
Jagger85 <andresfonsortega@yahoo.es>
2025-03-12 12:12:28 +0700

    Feat(blog/views): - Added dummy data for testing - Modified post detail view function - Added image

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path    
 from . import views
 
+
 urlpatterns = [
     path('', views.index, name='blog-index'),
     path('posts/', views.posts, name='all-posts'),
-    path('posts/<slug:slug>',views.post)
+    path('posts/<slug:slug>',views.post_detail, name='post-detail-page')
 ]
