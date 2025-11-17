7e0cb2066631cbf0ff4f518e78fc7ed90d1f9355
Jagger85 <andresfonsortega@yahoo.es>
2025-11-17 21:11:02 +0100

    Feat: Updated urls for the new config

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path    
 from . import views
 
+app_name = 'blog'
 
 urlpatterns = [
-    path('', views.index, name='blog-index'),
     path('posts/', views.posts, name='all-posts'),
     path('posts/<slug:slug>',views.post_detail, name='post-detail-page')
 ]
--- jagger_site/urls.py
+++ jagger_site/urls.py
@@ -18,8 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
-    path('speecher/', include('speecher.urls'))
 ]
